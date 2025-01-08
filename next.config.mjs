fde541d983069bd8616c22901241da700025f730
davonbl-bl <davonbl@gmail.com>
2025-01-08 10:54:30 -0500

    config the needed tools and logic to have the weatherapi working

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+import dotenv from 'dotenv'
+dotenv.config()
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        WEATHER_API_KEY: process.env.WEATHER_API_KEY
+    },
+    images: {
+        remotePatterns:[
+        {     
+            protocol: 'https',
+            hostname: 'cdn.weatherapi.com',
+            port: '',
+            pathname: '**',
+            search: '',
+            }
+        ] 
+    } 
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,21 @@
 import Image from "next/image";
+import { format, parse } from "date-fns";
 
-export default function Home() {
+const fetchData = async() => {
+  try {
+    const weatherURL = 'https://api.weatherapi.com/v1/'
+    const fetchForecast = await fetch(`${weatherURL}forecast.json?key=${process.env.WEATHER_API_KEY}&q=auto:ip&days=1`)
+    const data = await fetchForecast.json()
+    return data
+  } catch (error) {
+    console.error(error)
+    
+  }
+}
+
+export default async function Home() {
+  const getForecast = await fetchData()
+  console.log('here is the current forecast: ', getForecast)
   return (
     <div>
     </div>
